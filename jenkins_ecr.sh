938185456c1494f8bc50363bafc1b27963990e1b
yuleetea <yuleetea@gmail.com>
2022-04-12 12:24:49 -0400

    fixed access key

--- jenkins_ecr.sh
+++ jenkins_ecr.sh
@@ -2,8 +2,8 @@
 ecs-cli configure --cluster ec2-tutorial --default-launch-type EC2 \
 --config-name ec2-tutorial --region us-east-1
 
-ecs-cli configure profile --access-key $AKIAVNZW5JURADSZBM23 \
---secret-key $6w4jJN/aUCTz87w3C0DF3GnjqthKBony9Kf81kQp \
+ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID \
+--secret-key $AWS_SECRET_ACCESS_KEY \
 --profile-name ec2-tutorial-profile
 
 ecs-cli up --keypair ec2-docker --capability-iam --size 2 --instance-type t2.micro \

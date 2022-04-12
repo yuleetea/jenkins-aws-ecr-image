68c05d7607c18081c9e7dac778a03a463069a573
chitravelayutham <63082052+chitravelayutham@users.noreply.github.com>
2022-04-12 09:25:46 -0400

    Update jenkins_ecr.sh

--- jenkins_ecr.sh
+++ jenkins_ecr.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#ecs-cli configure --cluster ec2-tutorial --default-launch-type EC2 \
-#--config-name ec2-tutorial --region us-east-1
+ecs-cli configure --cluster ec2-tutorial --default-launch-type EC2 \
+--config-name ec2-tutorial --region us-east-1
 
-#ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID \
-#--secret-key $AWS_SECRET_ACCESS_KEY \
-#--profile-name ec2-tutorial-profile
+ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID \
+--secret-key $AWS_SECRET_ACCESS_KEY \
+--profile-name ec2-tutorial-profile
 
-#ecs-cli up --keypair test-jenkins-aws-ecr --capability-iam --size 2 --instance-type t2.micro \
-#--cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile --port 5000
+ecs-cli up --keypair test-jenkins-aws-ecr --capability-iam --size 2 --instance-type t2.micro \
+--cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile --port 5000
 
-#sleep 60
+sleep 60
 
-#ecs-cli compose up --create-log-groups --cluster-config ec2-tutorial \
-#--ecs-profile ec2-tutorial-profile
+ecs-cli compose up --create-log-groups --cluster-config ec2-tutorial \
+--ecs-profile ec2-tutorial-profile
 
-#ecs-cli ps --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
+ecs-cli ps --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
 
-ecs-cli down --force --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
+#ecs-cli down --force --cluster-config ec2-tutorial --ecs-profile ec2-tutorial-profile
